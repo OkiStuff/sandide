b65124a19eae775c9b84de85ea1943769e24c9e6
OkiStuff <40648091+OkiStuff@users.noreply.github.com>
2020-08-04 12:51:38 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SandIDE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("you thought i would of make a ide???");
+            
+            string userName = Console.ReadLine();
+        }
+    }
+}
